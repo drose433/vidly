b103c0472ad8fd166193028911bb9b9df98167fc
drose433 <miticjovan7@gmail.com>
2023-05-15 15:17:05 +0200

    dodata joi validacija

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Joi from 'joi-browser';
 import Input from './common/input';
 
 class LoginForm extends Component {
@@ -7,35 +8,55 @@ class LoginForm extends Component {
         errors: {}
     }
 
-    username = React.createRef();
+    schema = {
+        username: Joi.string().required(),
+        password: Joi.string().required()
+    };
 
    validate = () =>{
+
+    const result = Joi.validate(this.state.account, this.schema, { abortEarly: false })
+    console.log(result);
     const errors = {};
 
     const { account } = this.state;
     if(account.username.trim() === ''){
-        errors.username = 'Username is requied';
+        errors.username = 'Username is required';
     }
     if(account.password.trim() === ''){
-        errors.password = 'Password is requied';
+        errors.password = 'Password is required';
     }
 
     return Object.keys(errors).length === 0 ? null : errors;
    };
 
+   validateProperty = ({name, value}) =>{
+        if(name === 'username'){
+            if(value.trim() === '') return 'Username is required';
+        }
+        if(name === 'password'){
+            if(value.trim() === '') return 'Password is required';
+        }
+   };
+
     handleSubmit = e =>{
         e.preventDefault();
        const errors = this.validate();
-       console.log(errors);
+      
        this.setState({ errors: errors || {} });
        if(errors) return;
         console.log('Submited');
     };
 
     handleChange = ({ currentTarget: input }) =>{
+        const errors = {...this.state.errors};
+        const errorMessage = this.validateProperty(input);
+        if(errorMessage) errors[input.name] = errorMessage;
+        else delete errors[input.name];
+
         const account = {...this.state.account};
         account[input.name] = input.value;
-        this.setState({ account });
+        this.setState({ account, errors });
     }
     render() { 
         const { account, errors } = this.state;
